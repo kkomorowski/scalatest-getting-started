ff5aca42507f10fd2492a0d2c1d6c59b669a271d
Krzysztof Komorowski <krzysztof.r.komorowski@gmail.com>
2024-02-22 21:20:02 +0100

    Tests with response body parsed from JSON to an object

--- build.sbt
+++ build.sbt
@@ -1,3 +1,11 @@
 scalaVersion := "3.3.1"
+
+val circeVersion = "0.14.6"
+val sttpVersion = "3.9.3"
+
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.17" % "test"
-libraryDependencies += "com.softwaremill.sttp.client3" %% "core" % "3.9.3"
+libraryDependencies += "com.softwaremill.sttp.client3" %% "core" % sttpVersion
+libraryDependencies += "com.softwaremill.sttp.client3" %% "circe" % sttpVersion
+libraryDependencies += "io.circe" %% "circe-core" % circeVersion
+libraryDependencies += "io.circe" %% "circe-generic" % circeVersion
+libraryDependencies += "io.circe" %% "circe-parser" % circeVersion
---
+++ src/test/scala/GitHubUserDetailsSpec.scala
@@ -0,0 +1,44 @@
+import org.scalatest.GivenWhenThen
+import org.scalatest.featurespec.AnyFeatureSpec
+import org.scalatest.matchers.must.Matchers
+import sttp.client3.circe.asJson
+import sttp.client3.quick.simpleHttpClient
+import sttp.client3.{UriContext, quickRequest}
+import sttp.model.StatusCode
+import sttp.model.StatusCode.{NotFound, Ok}
+
+class GitHubUserDetailsSpec extends AnyFeatureSpec with GivenWhenThen with Matchers:
+
+  import io.circe.generic.auto._
+
+  case class GitHubUserProfile(login: String, blog: String, email: Option[String])
+
+  Feature("GitHub User Profile Details"):
+
+    Scenario("GitHub User Details Check"):
+      Given("GitHub user exists")
+      val request = quickRequest
+        .get(uri"https://api.github.com/users/kkomorowski")
+        .response(asJson[GitHubUserProfile])
+      When("I send the request for a GitHub User Profile")
+      val response = simpleHttpClient.send(request)
+      Then("I get the response with status 'OK'")
+      response.code mustBe Ok
+      And("the response body contains correct user, blog and email address")
+      response.body.isRight mustBe true
+      val user = response.body.toOption.get
+      user.login mustBe "kkomorowski"
+      user.blog mustBe "https://hiquality.dev"
+      user.email mustBe empty
+
+    Scenario("GitHub User Details Check - Failing"):
+      Given("GitHub user does not exists")
+      val request = quickRequest
+        .get(uri"https://api.github.com/users/yx-oyn-qzg-dun2a7")
+        .response(asJson[GitHubUserProfile])
+      When("I send the request for a GitHub User Profile")
+      val response = simpleHttpClient.send(request)
+      info(response.body.toString)
+      Then("I get the response with status 'Not Found'")
+      response.code mustBe NotFound
+      response.body.isRight mustBe true
